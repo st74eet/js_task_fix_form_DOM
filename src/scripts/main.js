1eca18a04d6753800d129d3376d9c067b64ed85e
Iryna Shevkun <sevkunirina65@gmail.com>
2025-04-23 09:19:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+const divs = [...document.querySelectorAll('div')];
+let count = 0;
+
+divs.forEach((div, index) => {
+  const newLabel = document.createElement('label');
+
+  if (div.classList.contains('field')) {
+    const label = div.insertAdjacentElement('afterbegin', newLabel);
+
+    label.classList.add('field-label');
+    label.setAttribute('for', inputs[count].id);
+    label.textContent = inputs[count].name;
+
+    count++;
+  }
+});
+
+inputs.forEach((input) => {
+  input.setAttribute('placeholder', capitalize(input.name));
+});
+
+function capitalize(s) {
+  return String(s[0]).toUpperCase() + String(s).slice(1);
+}
